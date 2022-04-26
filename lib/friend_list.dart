c8c7b564dede6a07d5b61ee9d5eddd01acaf354c
dabuzida <dabuzida@gmail.com>
2022-04-26 18:06:05 +0900

    친구목록의 item생성

--- lib/friend_list.dart
+++ lib/friend_list.dart
@@ -1,4 +1,10 @@
+import 'dart:html';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import 'person.dart';
 
 class FriendList extends StatefulWidget {
   const FriendList({Key? key}) : super(key: key);
@@ -7,21 +13,26 @@ class FriendList extends StatefulWidget {
 }
 
 class _FriendListState extends State<FriendList> {
-  // @override
-  // void initState() {
-  //   print('hi');
-  //   Widget n = _tile('AMC Metreon 16', '135 4th St #3000', Icons.theaters);
-  //   _tile('AMC Metreon 16', '135 4th St #3000', Icons.theaters),
-  // }
-
+  final List _friendLIst = [
+    // {'name': '피카츄', 'image': 'xxix__', 'timeCreated': '2022년 10월 1일 02시 33분 23초'},
+    // {'name': '파이리', 'image': 'vix_v_', 'timeCreated': '2022년 12월 1일 02시 33분 23초'},
+    // {'name': '꼬북이', 'image': 'xivxix_vv_', 'timeCreated': '2022년 11월 1일 02시 33분 23초'},
+    // {'name': 'xuiu', 'image': 'xaaxevf', 'timeCreated': '2022년 11월 1일 02시 33분 23초'},
+  ];
   // Widget n = ListTile(title: Text('title'), subtitle: Text('subtitle'), leading: Icon(Icons.theaters));
   // Widget n = _tile('AMC Metreon 16', '135 4th St #3000', Icons.theaters);
 
   // List<ListTile> _list = [];
 
   // Widget _friendList = Column(children: [_list.]);
-
-/* 
+  String search = '';
+  List names = ['neo', 'booker', 'david', 'christine', 'dash', 'evan', 'huan', 'james', 'jadey', 'daniel'];
+  // List images = ['a.jpg', 'b.jpg', 'c.jpg', 'd.jpg', 'e.jpg', 'f.jpg', 'g.jpg', 'h.jpg', 'i.jpg'];
+  // 이미지는 랜덤 url로
+  // https://picsum.photos/50
+  // https://picsum.photos/id/237/50   // 0~1084
+  // https://picsum.photos/id/'${number}/50
+  /* 
     ListTile(
           title: Text(title,
               style: const TextStyle(
@@ -40,22 +51,41 @@ class _FriendListState extends State<FriendList> {
 
   @override
   Widget build(BuildContext context) {
+    print(_friendLIst);
     return Scaffold(
       appBar: AppBar(
+        elevation: 0,
         foregroundColor: const Color(0xFFFFFFFF),
         backgroundColor: const Color(0xFF000000),
-        title: const Text('친구'),
-        elevation: 0,
+        leading: const Center(
+          child: Text(
+            '친구',
+            style: TextStyle(fontSize: 20),
+          ),
+        ),
+        title: IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+        centerTitle: true,
         actions: <Widget>[
           IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
           IconButton(
               onPressed: () {
                 setState(() {
-                  // _friendList
+                  int imageId = Random().nextInt(1085);
+                  String _imageUri = 'https://picsum.photos/id/$imageId/100';
+                  String _name = names[Random().nextInt(names.length)];
+                  DateTime now = DateTime.now();
+                  DateFormat format = DateFormat('yyyy년 MM월 dd일 E HH시 mm분 ss.SSS초');
+                  String _formatted = format.format(now);
+                  // Person p1 = Person(name: 'name', image: 'image', timeCreated: formatted);
+                  _friendLIst.add({
+                    'image': _imageUri,
+                    'name': _name,
+                    'timeCreated': _formatted,
+                  });
                   // _tile('AMC Metreon 16', '135 4th St #3000', Icons.theaters);
                 });
               },
-              icon: const Icon(Icons.person_add_alt_1_outlined)),
+              icon: const Icon(Icons.person_add_alt_1_outlined)), // 친구추가버튼
           IconButton(onPressed: () {}, icon: const Icon(Icons.music_note_outlined)),
           IconButton(onPressed: () {}, icon: const Icon(Icons.settings_sharp)),
         ],
@@ -85,11 +115,65 @@ class _FriendListState extends State<FriendList> {
         // currentIndex: _selectedIndex,
         // onTap: _onItemTapped,
       ),
-      body: ListView.builder(
-          itemCount: 10,
-          itemBuilder: (BuildContext context, int index) {
-            return Text('dd');
-          }),
+      body: search.isEmpty ? normalMode() : searchMode(),
+    );
+  }
+
+  Widget normalMode() {
+    return ListView.builder(
+        itemCount: _friendLIst.length,
+        itemBuilder: (BuildContext context, int index) {
+          return _item(_friendLIst[_friendLIst.length - index - 1]['image'], _friendLIst[_friendLIst.length - index - 1]['name'], _friendLIst[_friendLIst.length - index - 1]['timeCreated'], index);
+        });
+  }
+
+  Widget searchMode() {
+    return ListView.builder(
+        // itemCount: vi.length,
+        itemBuilder: (BuildContext context, int index) {
+      return Text('sdfsdfsdf');
+    });
+  }
+
+  Widget _item(String image, String name, String timeCreated, int index) {
+    return TextButton(
+      onPressed: () {
+        setState(() {
+          _friendLIst.removeAt(_friendLIst.length - index - 1);
+        });
+      },
+      child: SizedBox(
+        height: 100,
+        child: Row(
+          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            SizedBox(width: 400, child: Image.network(image)),
+            SizedBox(
+              width: 300,
+              child: Text(
+                name,
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 30,
+                  fontWeight: FontWeight.w700,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Expanded(
+              child: Text(
+                timeCreated,
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w400,
+                ),
+                textAlign: TextAlign.right,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
--- lib/person.dart
+++ lib/person.dart
@@ -1,4 +1,15 @@
 class Person {
+  String name;
+  String image;
+  String timeCreated;
+  Person({
+    required this.name,
+    required this.image,
+    required this.timeCreated,
+  });
+}
+
+class Person2 {
   String? name;
   var image;
   String? timeCreated;
